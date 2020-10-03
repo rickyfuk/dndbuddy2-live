1a3fd948bb9509708daa98e2f61f19393dc6dcd7
digilau <rickyfukfuk@gmail.com>
2020-10-02 17:56:29 -0700

    modify the database password

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 const db = require('./models');
+require('dotenv').config();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -42,8 +43,12 @@ const apiRoute = require('./controllers/apiRoute');
 app.use(apiRoute);
 app.use(htmlRoute);
 
+// console.log(process.env.JAWSUSERNAME);
+// console.log(process.env.JAWSPASSWORD);
+// console.log(process.env.DATABASE);
+
 // sync the sequelize model
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
 	app.listen(PORT, function () {
 		console.log('App listening on PORT ' + PORT);
 	});
